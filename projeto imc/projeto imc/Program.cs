b2d3c2f2b48892bfb2158698ac108dec10ed0e4f
Kainan silva <leticia.santos9@aluno.senai.br>
2025-11-27 16:03:47 -0300

    projeto

---
+++ projeto imc/projeto imc/Program.cs
@@ -0,0 +1,30 @@
+﻿double pesoEmKg;
+double alturaEmMetros;
+double imc;
+
+Console.WriteLine("Digite o peso:");
+pesoEmKg = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite a atltura:");
+alturaEmMetros = Convert.ToDouble(Console.ReadLine());
+
+if (alturaEmMetros == 0)
+{
+    Console.WriteLine("Altura deve ser maior que zero");
+    return;
+}
+double alturaAoQuadrado = alturaEmMetros * alturaEmMetros;
+
+imc = pesoEmKg / alturaAoQuadrado;
+
+double imcFormatado = Math.Round(imc, 2);
+
+Console.WriteLine($"O Seu IMC calculado é: {imcFormatado}");
+
+if ( imcFormatado < = 29.9
+    {
+    Console.WriteLine("Magro");
+}
+else
+{
+    Console.WriteLine("Obesidade");
+}
